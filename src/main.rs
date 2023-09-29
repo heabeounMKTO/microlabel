a41a8223a5388e0cd0e48cde99306eb2acbcadc1
heabeounMKTO <heabeoun69zin@gmail.com>
2023-09-29 17:01:44 +0700

    stash

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use eframe::egui;
 
+
+const default_width: f32 = 1280.0;
+const default_height: f32 = 720.0;
 struct DaApp {
     name: String,
     version: f32
@@ -16,12 +19,13 @@ impl Default for DaApp {
 
 impl eframe::App for DaApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::Window::new("Da Image in Question").show(ctx, |ui| {
-            ui.image(egui::include_image!("/home/hbpopos/Downloads/gd.jpg"));
+        egui::SidePanel::right("Image in question").default_width(default_width*(4.0/5.0)).show(ctx, |image_panel|{
+            let mut image_viewer = image_panel.image(egui::include_image!("/home/hbpopos/Downloads/gd.jpg"));
         });
         egui::Window::new("Da Toolbar").show(ctx, |toolbar| {
             let draw_rect = toolbar.button("Draw Rect");
             let draw_pts = toolbar.button("Draw Point");
+            let delete_label = toolbar.button("Edit Poly");
             let next_img = toolbar.button("Next Image");
             let prev_img = toolbar.button("Previous Image");
             
@@ -38,7 +42,7 @@ impl eframe::App for DaApp {
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(1280.0, 720.0)),
+        initial_window_size: Some(egui::vec2(default_width, default_height)),
         ..Default::default()
     };
     eframe::run_native("microlabel", options, Box::new(|cc| {
